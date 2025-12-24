1a34daafbc31030f1d5b01110197c157631c24cc
David Trabish <davidtr1037@gmail.com>
2025-12-24 15:20:00 +0200

    updating

--- code/example.simple.c
+++ code/example.simple.c
@@ -1,8 +1,8 @@
 int main() {
-  char *p = malloc(2);
+  char *p = malloc(2); /*@ \label{line:example-malloc} @*/
   unsigned n = make_symbolic();
   assume(n < 2);
-  p[0] = 7;
+  p[0] = 7; /*@ \label{line:example-store} @*/
   if (p[n] == 7) {
     return 1;
   } else {
