09ff4e5a213127c737b5a73e90e4ca4ac6d7d0b6
David Trabish <davidtr1037@gmail.com>
2026-05-27 11:01:40 +0800

    updating

--- code/overview.c
+++ code/overview.c
@@ -3,9 +3,9 @@
 #include <stdlib.h>
 
 int main() {
-    char *p = malloc(10);
+    char *a = malloc(10);
     unsigned n = klee_make_symbolic_int32();
     klee_assume(n < 10);
-    p[n] = 7;
+    a[n] = 7;
     return 0;
 }
